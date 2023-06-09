6ce9f725e93288b73791467230cc8c91e9fe198a
HappyFaceFriend <yjl980730@gmail.com>
2023-06-10 03:49:18 +0900

    Added some TODOs here and there

--- Ramensoup/src/Ramensoup/Core/EventQueue.h
+++ Ramensoup/src/Ramensoup/Core/EventQueue.h
@@ -13,6 +13,7 @@ namespace Ramensoup
 		{
 			s_Queue.push_back(std::forward<T>(event));
 		}
+		//TODO : Make specific Flush for specific event type, so caller can make different callbacks for different events
 		static void Flush(Event::EventCallbackFunc callback)
 		{
 			for (T& event : s_Queue)
--- Ramensoup/src/Ramensoup/Core/Layer.h
+++ Ramensoup/src/Ramensoup/Core/Layer.h
@@ -13,6 +13,8 @@ namespace Ramensoup
 		virtual void OnAttach() {}
 		virtual void OnDetach() {}
 		virtual void OnUpdate() {}
+		//TODO : To remove EventDispatcher, Layer should implement all kinds of eventHandlers, specific to type.
+		//		 As a result, HandleEvent() should dispatch events.
 		virtual void HandleEvent(Event& event) {}
 
 		inline const std::string& GetName() const { return m_DebugName; }
